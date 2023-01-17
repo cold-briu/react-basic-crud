9acd6b6a5dd01daf2670aa6c7d9d9fe81ad27804
Andrés Fernández <andresavena19@gmail.com>
2023-01-17 14:25:15 -0500

    feat: basic react template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// importar librerías y otros archivos
+import React from "react"
+
+// declarar componente
+const App = () => {
+	return <p> Soy App! </p>
+}
+
+// exportar componente
+export default App // default export 
+
+// export { // named export
+// 	una,
+// 	dos,
+// 	tres
+// }
