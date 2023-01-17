07eb1a71fd5579ab0f981a5b712b0b787537c83c
Andrés Fernández <andresavena19@gmail.com>
2023-01-17 14:38:40 -0500

    feat: added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
-	return <p>navbar</p>
+	return (
+		<>
+			<nav className="navbar navbar-expand-sm bg-light">
+				<div className="container-fluid">
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+				</div>
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link className="nav-link" to="/" >
+								Home
+							</Link>
+						</li>
+
+					</ul>
+				</div>
+			</nav>
+			<Outlet />
+		</>
+	)
 }
 
 export default Navbar
