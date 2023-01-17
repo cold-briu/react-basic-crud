d2ab34953e1a252509e1caca33fc68c1b621ce3a
Andrés Fernández <andresavena19@gmail.com>
2023-01-17 15:07:39 -0500

    feat: controlled user form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,6 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 
 const UserForm = () => {
+	const [id, setId] = useState("")
+	const [username, setUsername] = useState("")
+	const [name, setName] = useState("")
+	const [age, setAge] = useState("")
+
+	const handleId = (e) => {
+		setId(e.target.value)
+	}
+
+	const handleUsername = (e) => {
+		setUsername(e.target.value)
+	}
+
+	const handleName = (e) => {
+		setName(e.target.value)
+	}
+
+	const handleAge = (e) => {
+		setAge(e.target.value)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const user = {
+			id, username, name, age
+		}
+		console.log(user);
+	}
+
+
 	return (
 		<div className="container">
 			<div className="row">
@@ -11,11 +41,27 @@ const UserForm = () => {
 
 				<div className="col-6 pt-4">
 					<form>
-						<input placeholder="id" type="text" className="form-control mb-2" />
-						<input placeholder="username" type="text" className="form-control mb-2" />
-						<input placeholder="name" type="text" className="form-control mb-2" />
-						<input placeholder="age" type="text" className="form-control mb-2" />
-						<button className="btn btn-success">
+						<input
+							placeholder="id" type="text" className="form-control mb-2"
+							onChange={handleId} value={id}
+						/>
+
+						<input
+							placeholder="username" type="text" className="form-control mb-2"
+							onChange={handleUsername} value={username}
+						/>
+
+						<input
+							placeholder="name" type="text" className="form-control mb-2"
+							onChange={handleName} value={name}
+						/>
+
+						<input
+							placeholder="age" type="text" className="form-control mb-2"
+							onChange={handleAge} value={age}
+						/>
+
+						<button className="btn btn-success" onClick={handleSubmit}>
 							submit
 						</button>
 					</form>
